4bf2def76413b12a564bce19835857af44f9869c
lovekush rajput <lovekushrazput143@gmail.com>
2022-02-10 17:12:00 +0530

    block-BJaaiq

--- block-BJaaiq/code/index.js
+++ block-BJaaiq/code/index.js
@@ -1,12 +1,32 @@
-document.addEventListener("DOMContentLoaded", (event) => {
-   alert( `The content of the DOM is loaded`)
-  });
-
 let ul = document.querySelector('ul')
-quotes.forEach((elm) => {
-    let li = document.createElement('li')
-    let  p =  document.createElement('p')
-    li.innerText = elm.quoteText
-    p.innerText = elm.quoteAuthor
-    ul.append(li,p)
-});
+
+let max = 3
+let count  = 0
+
+function quote(){
+    for(let i=0; i<max ; i++){
+        count = count + 1
+        let li = document.createElement('li')
+        let blockquote =  document.createElement('blockquote')
+        blockquote.innerText = quotes[count].quoteText
+        let cite =  document.createElement('cite')
+        cite.innerText = quotes[count].quoteAuthor
+        li.append(blockquote,cite)
+        ul.append(li)
+    }
+}
+
+window.addEventListener("scroll",()=>{
+    let scrollTop = document.documentElement.scrollTop
+    let clientHeight = document.documentElement.clientHeight
+    let scrollHeight = document.documentElement.scrollHeight
+    if(scrollTop + clientHeight >= scrollHeight && count < quotes.length){
+        quote()
+    }
+})
+
+document.addEventListener("DOMContentLoaded", () => {
+    alert( `The content of the DOM is loaded`)
+    quote()
+   });
+
